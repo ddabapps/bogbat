87036669cb56e4f053f85eef513001e7084bb0c0
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2023-04-28 23:57:09 +0100

    Add program skeleton to BogBat.dpr
    * Fails if not built for Windows target
    
    * Reports memory leaks in debug mode.
    
    * Adds message and requires enter to be pressed at end of program iff:
      * debug build and
      * running under IDE debugger
    
    * Adds catch all exception handling.

--- src/BogBat.dpr
+++ src/BogBat.dpr
@@ -1,10 +1,45 @@
 program BogBat;
 
+{$IF not Defined(MSWINDOWS)}
+{$MESSAGE FATAL 'Can only be compiled as a Windows program'}
+{$ENDIF}
+
 {$APPTYPE CONSOLE}
 
 {$Resource *.res}
 
+uses
+  System.SysUtils;
 
 begin
+
+  {$IF Defined(DEBUG)}
+  ReportMemoryLeaksOnShutdown := True;
+  {$ENDIF}
+
+  try
+
+    // User must press enter to close the program when running a debug build
+    // under Delphi debugger
+    {$IF Defined(DEBUG)}
+    {$WARN SYMBOL_PLATFORM OFF}
+    if DebugHook <> 0 then
+    begin
+      Writeln;
+      Writeln('Press enter to end the program');
+      Readln;
+    end;
+    {$WARN SYMBOL_PLATFORM ON}
+    {$ENDIF}
+
+  except
+    // Catch any uncaught exceptions
+    on E: Exception do
+    begin
+      Writeln('Uncaught exception:');
+      Writeln(E.ClassName + ': ' + E.Message);
+    end;
+  end;
+
 end.
 
